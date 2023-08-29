a9fa66585346262379ba5dcf7761808fb8959a9a
Bokjihee <bokjihee@naver.com>
2023-08-29 12:41:26 +0900

    배틀만들기 변경

--- changePage.js
+++ changePage.js
@@ -3,12 +3,22 @@ function changePage(url){
 }
 
 function changeMenu(url, num){
-    window.location.href = url;
-    let Else = document.querySelectorAll('#menu-bar > div > a');
-    for (let i=0; i<Else.length; i++) {
-        Else[i].style.fontWeight = 'normal';
-    }
+    console.log("눌림");
+    // let Else = document.querySelectorAll('#menu-bar > div > a');
+    // console.log(Else.length);
+    // for (let i=0; i<Else.length; i++) {
+    //     Else[i].style.color = 'red';
+    // }
+    // let test = document.getElementById("test");
+    // test.style.color = 'red';
+    document.addEventListener("DOMContentLoaded", function() {
+        console.log("실행");
+        let test = document.getElementById("test");
+        test.style.color = 'red';
+    });
+    window.location.href = url; //스타일 바뀐 뒤에 페이지 불러와야함
+    // let changeA = document.querySelector(`#menu-bar > div > a:nth-child(${num})`);
+    // changeA.style.fontWeight = 'bold';
     
-    let changeA = document.querySelector(`#menu-bar > div > a:nth-child(${num})`);
-    changeA.style.fontWeight = 'bold';
+    console.log("실행");
 }
